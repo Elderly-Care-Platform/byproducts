3519e7de022ab54e5580dbc334adcb5936a90c2c
sanjukta <sanjuktas@beautifulyears.com>
2015-12-01 17:57:57 +0530

    new credential for ecom express

--- site/src/main/java/com/beautifulyears/BYConstants.java
+++ site/src/main/java/com/beautifulyears/BYConstants.java
@@ -14,8 +14,8 @@ public class BYConstants {
 	/*for dev*/
 //	public static final ImmutableList<String> ADMIN_EMAILS = ImmutableList.of();
 //	public static final String SITE_URL = "http://dev.beautifulyears.com";
-//	
-//	
+	
+	
 	
 	/* for production */
 //	public static final ImmutableList<String> ADMIN_EMAILS = ImmutableList.of(
@@ -27,7 +27,8 @@ public class BYConstants {
 	public static final String SITE_URL = "http://localhost:8080/ROOT";
 	
 	
-	public static final String LOGISTIC_API = "http://ecomm.prtouch.com";
-	public static final String LOGISTIC_USERNAME = "ecomexpress";
-	public static final String LOGISTIC_PASSWORD = "Ke%243c%404oT5m6h%23%24";
+	public static final String LOGISTIC_API = "http://api.ecomexpress.in";
+	public static final String LOGISTIC_USERNAME = "beautiful";
+	public static final String LOGISTIC_PASSWORD = "b1e2a3u4t5";
+	
 }
