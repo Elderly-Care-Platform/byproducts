9bc0356f8dac825e746f59062950ee1cffd7ecfa
Nitin Jain <nitin.j@beautifulyears.com>
2015-12-17 11:30:28 +0530

    changing email structure

---
+++ site/src/main/java/com/beautifulyears/domain/logistic/EmailOrderObject.java
@@ -0,0 +1,208 @@
+/**
+ * 
+ */
+package com.beautifulyears.domain.logistic;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author Nitin
+ *
+ */
+public class EmailOrderObject {
+	private String orderNumber;
+	private Date submitDate;
+	private String customerName;
+	private String shippingAddr1;
+	private String shippingAddr2;
+	private BigDecimal subTotal;
+	private BigDecimal total;
+	private Float totalTax = 0f;
+	private BigDecimal totalDeliveryCharges;
+	private String orderPaymentType = "PPD";
+	private List<OrderItem> orderItems = new ArrayList<EmailOrderObject.OrderItem>();
+
+	public String getOrderPaymentType() {
+		return orderPaymentType;
+	}
+
+	public void setOrderPaymentType(String orderPaymentType) {
+		this.orderPaymentType = orderPaymentType;
+	}
+
+	public List<OrderItem> getOrderItems() {
+		return orderItems;
+	}
+
+	public void setOrderItems(List<OrderItem> orderItems) {
+		this.orderItems = orderItems;
+	}
+
+	public static class OrderItem {
+		private String itemName;
+		private String itemId;
+		private Integer quantity;
+		private Float price = 0f;
+		private BigDecimal deliveryCharge;
+		private Float taxAmount = 0f;
+		private String taxCode;
+		private String awbNumber;
+		private String barCodeImageDataPath;
+
+		public String getBarCodeImageDataPath() {
+			return barCodeImageDataPath;
+		}
+
+		public void setBarCodeImageDataPath(String barCodeImageDataPath) {
+			this.barCodeImageDataPath = barCodeImageDataPath;
+		}
+
+		public String getAwbNumber() {
+			return awbNumber;
+		}
+
+		public void setAwbNumber(String awbNumber) {
+			this.awbNumber = awbNumber;
+		}
+
+		public String getTaxCode() {
+			return taxCode;
+		}
+
+		public void setTaxCode(String taxCode) {
+			this.taxCode = taxCode;
+			try {
+				this.setTaxAmount(Float.parseFloat(taxCode) * this.getPrice()
+						/ 100);
+			} catch (Exception e) {
+				this.setTaxAmount(0f);
+			}
+
+		}
+
+		public String getItemName() {
+			return itemName;
+		}
+
+		public void setItemName(String itemName) {
+			this.itemName = itemName;
+		}
+
+		public String getItemId() {
+			return itemId;
+		}
+
+		public void setItemId(String itemId) {
+			this.itemId = itemId;
+		}
+
+		public Integer getQuantity() {
+			return quantity;
+		}
+
+		public void setQuantity(Integer quantity) {
+			this.quantity = quantity;
+		}
+
+		public Float getPrice() {
+			return price;
+		}
+
+		public void setPrice(Float price) {
+			this.price = price;
+		}
+
+		public BigDecimal getDeliveryCharge() {
+			return deliveryCharge;
+		}
+
+		public void setDeliveryCharge(BigDecimal deliveryCharge) {
+			this.deliveryCharge = deliveryCharge;
+		}
+
+		public Float getTaxAmount() {
+			return taxAmount;
+		}
+
+		public void setTaxAmount(Float taxAmount) {
+			this.taxAmount = taxAmount;
+		}
+
+	}
+
+	public String getOrderNumber() {
+		return orderNumber;
+	}
+
+	public void setOrderNumber(String orderNumber) {
+		this.orderNumber = orderNumber;
+	}
+
+	public Date getSubmitDate() {
+		return submitDate;
+	}
+
+	public void setSubmitDate(Date submitDate) {
+		this.submitDate = submitDate;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public String getShippingAddr1() {
+		return shippingAddr1;
+	}
+
+	public void setShippingAddr1(String shippingAddr1) {
+		this.shippingAddr1 = shippingAddr1;
+	}
+
+	public String getShippingAddr2() {
+		return shippingAddr2;
+	}
+
+	public void setShippingAddr2(String shippingAddr2) {
+		this.shippingAddr2 = shippingAddr2;
+	}
+
+	public BigDecimal getSubTotal() {
+		return subTotal;
+	}
+
+	public void setSubTotal(BigDecimal bigDecimal) {
+		this.subTotal = bigDecimal;
+	}
+
+	public BigDecimal getTotal() {
+		return total;
+	}
+
+	public void setTotal(BigDecimal bigDecimal) {
+		this.total = bigDecimal;
+	}
+
+	public Float getTotalTax() {
+		return totalTax;
+	}
+
+	public void setTotalTax(Float totalTax) {
+		this.totalTax = totalTax;
+	}
+
+	public BigDecimal getTotalDeliveryCharges() {
+		return totalDeliveryCharges;
+	}
+
+	public void setTotalDeliveryCharges(BigDecimal totalDeliveryCharges) {
+		this.totalDeliveryCharges = totalDeliveryCharges;
+	}
+
+}
--- site/src/main/java/com/beautifulyears/service/email/ExtendEmailServiceImpl.java
+++ site/src/main/java/com/beautifulyears/service/email/ExtendEmailServiceImpl.java
@@ -1,6 +1,7 @@
 package com.beautifulyears.service.email;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.HashMap;
 
 import javax.annotation.Resource;
@@ -9,6 +10,9 @@ import org.broadleafcommerce.common.email.service.EmailService;
 import org.broadleafcommerce.common.email.service.EmailServiceImpl;
 import org.broadleafcommerce.common.email.service.info.EmailInfo;
 import org.broadleafcommerce.common.email.service.message.Attachment;
+import org.broadleafcommerce.common.payment.PaymentType;
+import org.broadleafcommerce.core.order.domain.DiscreteOrderItem;
+import org.broadleafcommerce.core.order.domain.DiscreteOrderItemImpl;
 import org.broadleafcommerce.core.order.domain.Order;
 import org.broadleafcommerce.core.order.domain.OrderItem;
 import org.springframework.beans.BeansException;
@@ -16,6 +20,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Service;
 
+import com.beautifulyears.domain.logistic.EmailOrderObject;
+import com.beautifulyears.sample.catalog.domain.ExtendProductImpl;
 import com.beautifulyears.sample.fedExOrder.domain.OrderTrackingInfo;
 import com.beautifulyears.util.BarcodeUtil;
 
@@ -47,29 +53,8 @@ public class ExtendEmailServiceImpl implements ApplicationContextAware, ExtendEm
   public void sendOrderConfirmation(Order order, OrderTrackingInfo orderTrackingInfo,
       String emailAddress) throws IOException {
     HashMap<String, Object> props = new HashMap<String, Object>();
-    StringBuffer name = new StringBuffer("");
-    name.append(order.getOrderAttributes().get("firstName"));
-    name.append(" ");
-    name.append(order.getOrderAttributes().get("lastName"));
-    props.put("name", name.toString());
-    
-    
-    StringBuffer address1 = new StringBuffer("");
-    address1.append(order.getOrderAttributes().get("AddressLine1"));
-    address1.append(order.getOrderAttributes().get(" ,"));
-    props.put("address1", address1.toString());
-    
-    StringBuffer address2 = new StringBuffer("");
-    address2.append(order.getOrderAttributes().get("AddressLine2"));
-    address2.append(" ,");
-    address2.append(order.getOrderAttributes().get("City"));
-    address2.append(" , mob:");
-    address2.append(order.getOrderAttributes().get("Phone"));
-    address2.append(" ,");
-    props.put("address2", address2.toString());
-    
-    props.put("order", order);
-    props.put("orderTrackingInfo", orderTrackingInfo);
+    EmailOrderObject emailObj = getEmailObject(order,null);
+    props.put("order", emailObj);
     
     EmailInfo emailInfo = getOrderConfirmationEmailInfo();
     StringBuffer file = new StringBuffer();
@@ -195,38 +180,10 @@ public void sendOrderItemConfirmation(Order order,
 		throws IOException {
 	{
 	    HashMap<String, Object> props = new HashMap<String, Object>();
-	    props.put("order", order);
-	    props.put("orderItem", item);
+	    EmailOrderObject emailObj = getEmailObject(order,item);
 	    
-	    StringBuffer name = new StringBuffer("");
-	    name.append(order.getOrderAttributes().get("firstName"));
-	    name.append(" ");
-	    name.append(order.getOrderAttributes().get("lastName"));
-	    props.put("name", name.toString());
-	    
-	    
-	    StringBuffer address1 = new StringBuffer("");
-	    address1.append(order.getOrderAttributes().get("AddressLine1"));
-	    address1.append(order.getOrderAttributes().get(" ,"));
-	    props.put("address1", address1.toString());
-	    
-	    StringBuffer address2 = new StringBuffer("");
-	    address2.append(order.getOrderAttributes().get("AddressLine2"));
-	    address2.append(" ,");
-	    address2.append(order.getOrderAttributes().get("City"));
-	    address2.append(" , mob:");
-	    address2.append(order.getOrderAttributes().get("Phone"));
-	    address2.append(" ,");
-	    props.put("address2", address2.toString());
-	    System.out.println("finding item awb ========");
-	    if(null != item.getOrderItemAttributes()   &&  null != item.getOrderItemAttributes().get("awbNumber")){
-	    	String awb = item.getOrderItemAttributes().get("awbNumber").getValue();
-	    	props.put("awbNumber", awb);
-	    	System.out.println("item's awb = "+awb);
-	    	String barCodeImageDataPath = BarcodeUtil.getBarcodeImage(awb);
-	    	System.out.println("barcode data = "+barCodeImageDataPath);
-	        props.put("barcodeImgData", barCodeImageDataPath);
-	    }
+	    props.put("order", emailObj);
+	    props.put("orderItem", emailObj.getOrderItems().get(0));
 	    
 	    StringBuffer file = new StringBuffer();
 	    EmailInfo emailInfo = getOrderItemConfirmationEmailInfo();
@@ -262,5 +219,74 @@ public void sendOrderCancellationAdmin(Order order, String emailAddress)
 		throws IOException {
 	// TODO Auto-generated method stub
 	
+}
+
+
+private EmailOrderObject getEmailObject(Order order, OrderItem orderItem){
+EmailOrderObject emailObj = new EmailOrderObject();
+    
+    StringBuffer name = new StringBuffer("");
+    name.append(order.getOrderAttributes().get("firstName"));
+    name.append(" ");
+    name.append(order.getOrderAttributes().get("lastName"));
+//    props.put("name", name.toString());
+    emailObj.setCustomerName(name.toString());
+    emailObj.setSubmitDate(order.getSubmitDate());
+    
+    
+    StringBuffer address1 = new StringBuffer("");
+    address1.append(order.getOrderAttributes().get("AddressLine1"));
+    address1.append(" ,");
+//    props.put("address1", address1.toString());
+    emailObj.setShippingAddr1(address1.toString());
+    emailObj.setOrderNumber(order.getOrderNumber());
+    emailObj.setSubTotal(order.getSubTotal().getAmount());
+    emailObj.setTotal(order.getTotal().getAmount());
+    emailObj.setTotalDeliveryCharges(order.getTotalShipping().getAmount());
+    
+    StringBuffer address2 = new StringBuffer("");
+    address2.append(order.getOrderAttributes().get("AddressLine2"));
+    address2.append(" ,");
+    address2.append(order.getOrderAttributes().get("City"));
+    address2.append(" , mob:");
+    address2.append(order.getOrderAttributes().get("Phone"));
+    address2.append(" ,");
+//    props.put("address2", address2.toString());
+    emailObj.setShippingAddr2(address2.toString());
+    for(OrderItem item : order.getOrderItems()){
+    	if(orderItem != null && !orderItem.getId().equals(item.getId())){
+    		continue;
+    	}
+    	EmailOrderObject.OrderItem itemObj = new EmailOrderObject.OrderItem();
+    	ExtendProductImpl product = (ExtendProductImpl) ((DiscreteOrderItem) item)
+				.getProduct();
+    	itemObj.setDeliveryCharge(new BigDecimal(product.getProductDeliveryCharges()));
+    	itemObj.setItemId(item.getId().toString());
+    	itemObj.setItemName(item.getName());
+    	itemObj.setPrice(item.getTotalPrice().getAmount().floatValue());
+    	itemObj.setQuantity(item.getQuantity());
+    	itemObj.setTaxCode(((DiscreteOrderItem) item).getSku().getTaxCode());
+    	emailObj.setTotalTax(emailObj.getTotalTax() + itemObj.getTaxAmount());
+    	
+    	if(null != item.getOrderItemAttributes()   &&  null != item.getOrderItemAttributes().get("awbNumber")){
+	    	String awb = item.getOrderItemAttributes().get("awbNumber").getValue();
+	    	itemObj.setAwbNumber(awb);
+	    	String barCodeImageDataPath = BarcodeUtil.getBarcodeImage(awb);
+	    	System.out.println("barcode data = "+barCodeImageDataPath);
+	    	itemObj.setBarCodeImageDataPath(barCodeImageDataPath);
+	    }
+    	
+    	
+    	emailObj.getOrderItems().add(itemObj);
+    }
+	if (null != order.getPayments() && null != order.getPayments().get(0)
+			&& null != order.getPayments().get(0).getType()
+			&& order.getPayments().get(0).getType() == PaymentType.COD) {
+		emailObj.setOrderPaymentType("COD");
+	}
+	
+	
+	
+	return emailObj;
 }
 }
