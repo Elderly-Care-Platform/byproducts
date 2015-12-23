c851c134240c559380b3284fef993a1dc2ff0850
Nitin Jain <nitin.j@beautifulyears.com>
2015-12-23 20:00:19 +0530

    registration related changes and refactoring

---
+++ site/src/main/java/com/beautifulyears/workflow/SendOrderEmailRollbackHandler.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package com.beautifulyears.workflow;
+
+import java.util.Map;
+
+import org.broadleafcommerce.core.checkout.service.workflow.CheckoutSeed;
+import org.broadleafcommerce.core.order.domain.Order;
+import org.broadleafcommerce.core.order.domain.OrderItem;
+import org.broadleafcommerce.core.workflow.Activity;
+import org.broadleafcommerce.core.workflow.ProcessContext;
+import org.broadleafcommerce.core.workflow.state.RollbackFailureException;
+import org.broadleafcommerce.core.workflow.state.RollbackHandler;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Nitin
+ *
+ */
+@Component("blSendOrderEmailRollbackHandler")
+public class SendOrderEmailRollbackHandler  implements RollbackHandler<CheckoutSeed> {
+
+	@Override
+	public void rollbackState(
+			Activity<? extends ProcessContext<CheckoutSeed>> activity,
+			ProcessContext<CheckoutSeed> processContext,
+			Map<String, Object> stateConfiguration)
+			throws RollbackFailureException {
+		System.out.println("Rollback for order email send activity called");
+		
+	}
+
+}
