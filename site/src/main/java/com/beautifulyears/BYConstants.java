f2ea97562fd9a2f9545327261fbadef42403dfdd
Nitin Jain <nitin.j@beautifulyears.com>
2016-02-11 15:11:48 +0530

    adding https

--- site/src/main/java/com/beautifulyears/BYConstants.java
+++ site/src/main/java/com/beautifulyears/BYConstants.java
@@ -14,17 +14,18 @@ public class BYConstants {
 	
 	/*for dev*/
 	public static final ImmutableList<String> ADMIN_EMAILS = ImmutableList.of("nitin.j@beautifulyears.com");
-	public static final String SITE_URL = "http://dev.beautifulyears.com";
+	public static final String SITE_URL = "https://dev.beautifulyears.com";
 	public static final String LOGISTIC_API = "http://ecomm.prtouch.com";
 	public static final String LOGISTIC_USERNAME = "ecomexpress";
 	public static final String LOGISTIC_PASSWORD = "Ke%243c%404oT5m6h%23%24";
 	
 	
 	
+	
 	/* for production */
 //	public static final ImmutableList<String> ADMIN_EMAILS = ImmutableList.of(
 //			"jharana.v@beautifulyears.com", "amarnath.m@beautifulyears.com ");
-//	public static final String SITE_URL = "http://beautifulyears.com";
+//	public static final String SITE_URL = "https://beautifulyears.com";
 //	public static final String LOGISTIC_API = "http://api.ecomexpress.in";
 //	public static final String LOGISTIC_USERNAME = "beautiful";
 //	public static final String LOGISTIC_PASSWORD = "b1e2a3u4t5";
