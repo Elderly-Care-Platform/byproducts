06da72ffa8429040a69ad3b4b6712af2b68475c9
Nitin Jain <nitin.j@beautifulyears.com>
2016-02-12 16:22:04 +0530

    checking for https error

--- site/src/main/java/com/beautifulyears/util/BarcodeUtil.java
+++ site/src/main/java/com/beautifulyears/util/BarcodeUtil.java
@@ -9,7 +9,9 @@ import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
 import com.beautifulyears.BYConstants;
@@ -23,11 +25,11 @@ public class BarcodeUtil {
 	public static String getBarcodeImage(String dataString) {
 		String barCodeImagePath = "";
 		try {
-			HttpClient httpclient = new DefaultHttpClient();
-
+//			HttpClient httpclient = new DefaultHttpClient();
+			CloseableHttpClient httpClient = HttpClients.createDefault();
 			HttpGet httpget = new HttpGet(BYConstants.SITE_URL
 					+ "/generateBarCode?barcodeString=" + dataString);
-			HttpResponse response = httpclient.execute(httpget);
+			HttpResponse response = httpClient.execute(httpget);
 			HttpEntity entity = response.getEntity();
 			barCodeImagePath = EntityUtils.toString(entity, "UTF-8");
 //			barCodeImagePath = BYConstants.SITE_URL+barCodeImagePath;
